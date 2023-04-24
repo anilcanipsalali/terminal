727b72e2420975d26015c30d50c5b6c0973cb9f2
anilcanipsalali <anilcan.ipsalali@gmail.com>
2023-04-24 11:42:36 +0300

    unused import deleted.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
-import config from '../../config.json';
 
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
